b8a8656256f715f2645f2337957075a8c6db3b48
Aleksandr <deadone.2018@inbox.ru>
2023-08-21 15:16:38 +0400

    Вторая задача

---
+++ №002/Program.cs
@@ -0,0 +1,14 @@
+﻿void Fillnumber3(int number ) {
+    int resualt = number%1000;
+    if (resualt>=100) {
+        int final = number%10;
+        Console.WriteLine(final);
+    }
+    else {
+        Console.WriteLine("Нет третьего числа");
+    }
+    }
+
+int numberA=int.Parse(Console.ReadLine());
+Fillnumber3(numberA);
+
